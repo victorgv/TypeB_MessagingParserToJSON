0bf0b20e9217418c186e5053b9c05406d4dcd632
Victor Garcia Velasco <victorgv@gmail.com>
2021-01-22 14:02:43 +0100

    preparando servicio windows

--- source/udmService.pas
+++ source/udmService.pas
@@ -3,13 +3,16 @@ unit udmService;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.SvcMgr, Vcl.Dialogs;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.SvcMgr, Vcl.Dialogs,uTServiceImplementation;
 
 type
   TdmService = class(TService)
     procedure ServiceExecute(Sender: TService);
+    procedure ServiceStart(Sender: TService; var Started: Boolean);
+    procedure ServiceStop(Sender: TService; var Stopped: Boolean);
   private
     { Private declarations }
+    fServiceImplementation: TServiceImplementation;
   public
     function GetServiceController: TServiceController; override;
     { Public declarations }
@@ -21,6 +24,8 @@ var
 implementation
 
 {$R *.dfm}
+
+
 
 procedure ServiceController(CtrlCode: DWord); stdcall;
 begin
@@ -39,6 +44,18 @@ begin
     ServiceThread.ProcessRequests(True);
     Sleep(1000);
   end;
+end;
+
+procedure TdmService.ServiceStart(Sender: TService; var Started: Boolean);
+begin
+  fServiceImplementation := TServiceImplementation.Create(self);
+  fServiceImplementation.ServiceExecute;
+end;
+
+procedure TdmService.ServiceStop(Sender: TService; var Stopped: Boolean);
+begin
+  fServiceImplementation.ServiceStop;
+  freeAndNil(fServiceImplementation);
 end;
 
 end.
