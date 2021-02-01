e58742688755b2713af9701dd3650ed0e58ffd70
Victor Garcia Velasco <victorgv@gmail.com>
2021-02-01 23:06:54 +0100

    added on dll parser of origin section

--- source/uCommon.pas
+++ source/uCommon.pas
@@ -6,8 +6,8 @@ uses System.JSON;
 
 type
   TProcessMessageFUNCTION = function(const p_message: String): TJSONObject; stdcall;
-  TDLLVersionFUNCTION = function: String; stdcall;
-  TDLLInfoFUNCTION = function: String; stdcall;
+  TDLLVersionFUNCTION = function: ShortString; stdcall;
+  TDLLInfoFUNCTION = function: ShortString; stdcall;
 
 implementation
 
